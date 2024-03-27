05902192eca1d2afc7dc6a8acb108757f4fbe849
Geoff <geoffchan23@gmail.com>
2024-03-27 09:37:05 -0400

    update to be more flexible

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ async function run() {
     const user = github.context.payload.sender.login;
     const sponsorTeamSlug = core.getInput('sponsor-team-slug', { required: true });
 
+    // Allowed labels for the sponsor team
+    const allowedLabels = core.getInput('allowed-labels', { required: true }).split(','); 
+
+
     // Get team members using GitHub's REST API
     const octokit = github.getOctokit(core.getInput('github-token', { required: true }));
     const { data: teamMembers } = await octokit.rest.teams.listMembersInOrg({
@@ -18,21 +22,24 @@ async function run() {
 
     // Check if the user who added the label is in the sponsor team
     if (teamMembers.some((member) => member.login === user)) {
-      // Remove the label
-      await octokit.rest.issues.removeLabel({
-        owner: repo.owner.login,
-        repo: repo.name,
-        issue_number: issueNumber,
-        name: addedLabel
-      });
+      // Check if the added label is within the allowed list
+      if (!allowedLabels.includes(addedLabel)) { 
+        // Remove the label if it's not allowed
+        await octokit.rest.issues.removeLabel({
+          owner: repo.owner.login,
+          repo: repo.name,
+          issue_number: issueNumber,
+          name: addedLabel
+        });
 
-      // (Optional) Send a notification 
-      await octokit.rest.issues.createComment({
-        owner: repo.owner.login,
-        repo: repo.name,
-        issue_number: issueNumber,
-        body: `@${user} Label changes by sponsor team members are not allowed. The label '${addedLabel}' has been removed.`
-      });
+        // (Optional) Send a notification 
+        await octokit.rest.issues.createComment({
+          owner: repo.owner.login,
+          repo: repo.name,
+          issue_number: issueNumber,
+          body: `@${user} Label changes by sponsor team members are restricted to the following labels: ${allowedLabels.join(', ')}. The label '${addedLabel}' has been removed.`
+        });
+      }
     }
   } catch (error) {
     core.setFailed(error.message);
