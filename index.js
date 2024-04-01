c1b7248a408fe52957ecdb9ce90c54b277da5d51
Geoff <geoffchan23@gmail.com>
2024-04-01 16:39:02 -0400

    clean up code and fix assignment cases

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const github = require('@actions/github');
 async function run() {
   console.log("Payload", github.context.payload);
   try {
-    const repo = github.context.payload.repository;
-    const issueNumber = github.context.eventName === 'issues' ? github.context.payload.issue.number : github.context.payload.number;
+    const repo = github.context.payload.repository.name;
+    const owner = github.context.payload.repository.owner.login;
+    const issue_number = github.context.eventName === 'issues' ? github.context.payload.issue.number : github.context.payload.number;
     const user = github.context.payload.sender.login;
 
     // Input handling (multiple teams, whitespace trimming)
@@ -32,7 +33,7 @@ async function run() {
       const {
         data: teamMembers
       } = await octokit.rest.teams.listMembersInOrg({
-        org: repo.owner.login,
+        org: owner,
         team_slug: teamSlug
       });
       allTeamMembers = allTeamMembers.concat(teamMembers);
@@ -43,45 +44,81 @@ async function run() {
     // Only apply to users from teams specified in `sponsor-team-slug`
     if (allTeamMembers.some((member) => member.login === user)) {
 
-      // --- Issue Actions ---
-      if (github.context.eventName === 'issues') { 
-
-        // --- Label Handling ---
-        if (github.context.eventName === 'issues' && (action === 'labeled' || action === 'unlabeled')) {
-          const labelName = github.context.payload.label.name;
-          if (!allowedLabels.includes(labelName)) {
-            if (action === 'labeled') {
-              await octokit.rest.issues.removeLabel({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, name: labelName});
-            } else if (action === 'unlabeled') {
-              await octokit.rest.issues.addLabels({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, labels: [labelName] });
-            }
-
-            // --- Output warning to sponsor ---
-            await octokit.rest.issues.createComment({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, body: `@${user} Sponsors can only use these labels: ${allowedLabels.join(', ')}.`});
+      // --- Label Handling ---
+      if (github.context.eventName === 'issues' && (action === 'labeled' || action === 'unlabeled')) {
+        const labelName = github.context.payload.label.name;
+        
+        if (!allowedLabels.includes(labelName)) {
+          if (action === 'labeled') {
+            await octokit.rest.issues.removeLabel({
+              owner,
+              repo,
+              issue_number,
+              name: labelName
+            });
+          } else if (action === 'unlabeled') {
+            await octokit.rest.issues.addLabels({
+              owner,
+              repo,
+              issue_number,
+              labels: [labelName]
+            });
           }
+
+          // --- Output warning to sponsor ---
+          await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number,
+            body: `@${user} Sponsors can only use these labels: ${allowedLabels.join(', ')}.`
+          });
         }
-
-      }
-
-      // --- PRs & Issues Actions ---
-      // --- Closing/Reopening/Assignment Handling ---
-      if (action === 'closed' || action === 'reopened' || action === 'assigned') {
+      } else {
+        // --- Closing/Reopening/Assignment Handling ---
+        const assignees = github.context.payload.assignee.login;
+  
         if (action === 'closed') {
-          await octokit.rest.issues.update({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, state: 'open'});
+          await octokit.rest.issues.update({
+            owner,
+            repo,
+            issue_number,
+            state: 'open'
+          });
         } else if (action === 'reopened') {
-          await octokit.rest.issues.update({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, state: 'closed'});
+          await octokit.rest.issues.update({
+            owner,
+            repo,
+            issue_number,
+            state: 'closed'
+          });
         } else if (action === 'assigned') {
-          //Currently not working as expected. If I assign an issue to myself it will unassign me. But if I assign the issue to someone else it does not unassign that person from the issue.
-          await octokit.rest.issues.removeAssignees({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, assignees: [user] });
+          await octokit.rest.issues.removeAssignees({
+            owner,
+            repo,
+            issue_number,
+            assignees
+          });
+        } else if (action === 'unassigned') {
+          await octokit.rest.issues.addAssignees({
+            owner,
+            repo,
+            issue_number,
+            assignees
+          });
         }
-
+  
         // --- Output warning to sponsor ---
-        await octokit.rest.issues.createComment({ owner: repo.owner.login, repo: repo.name, issue_number: issueNumber, body: `@${user} Sponsors are not allowed to close, reopen, or assign issues or pull requests.`});
+        await octokit.rest.issues.createComment({
+          owner,
+          repo,
+          issue_number,
+          body: `@${user} Sponsors are not allowed to close, reopen, or assign issues or pull requests.`
+        });
       }
-    }
+    } // Only apply to users from teams specified in `sponsor-team-slug`
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run(); 
+run();
